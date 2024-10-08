6e63ed4f180684b996e1865becfbbae0762de77a
inesmariao <63691309+inesmariao@users.noreply.github.com>
2024-10-07 20:56:45 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+// Importar dependencias (configurar en package.json)
+
+// Conexión a la Base de Datos
+
+// Crear el servidor node
+
+
+// Configurar cors para que acepte peticiones del frontend
+
